8fdaebbbd2c5f7fba94cca528efd32c1983d309e
Iñaki Aramburu <inaki.aramburur@alumni.mondragon.edu>
2020-06-30 13:13:40 +0200

    plataforma y personaje
    el scrit de pJ1 esta mal

---
+++ projet-x/Assets/Sprites/Personajes/PJ1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PJ1 : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+    public float fuerza = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        mover(fuerza);
+        
+
+        
+    }
+
+    public void mover(float fuerza)
+    {
+        //fuerza *= Timeout.deltaTime;
+
+        if (Input.GetKey("d"))//derecha
+        {
+            gameObject.transform.Traslate(fuerza, 0, 0);
+        }
+    }
+
+}
+

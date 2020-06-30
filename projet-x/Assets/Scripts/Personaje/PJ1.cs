090e270e31c9f2d4e665e5c1996b32b0f7197c8f
PesteNegra12 <63926745+PesteNegra12@users.noreply.github.com>
2020-06-30 14:30:25 +0200

    Movimiento
    El personaje se mueve de izquierda a derecha (He quitado el documento de variables globales por un fallo que he detectado)

---
+++ projet-x/Assets/Scripts/Personaje/PJ1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PJ1 : MonoBehaviour
+{
+    public float fuerza = 100f;
+    public float salto = 300f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        mover(fuerza);
+        
+    }
+
+    public void mover(float f)
+    {
+        if (Input.GetKey("a"))//Izquierda
+        {
+            gameObject.transform.Translate(-f * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey("d"))//Derecha
+        {
+            gameObject.transform.Translate(f * Time.deltaTime, 0, 0);
+        }
+    }
+
+}
+

faf87d7969900445581b77a4d85fb5e10f460e0c
PesteNegra12 <63926745+PesteNegra12@users.noreply.github.com>
2020-07-02 16:22:03 +0200

    Varias formas de movimiento + Salto
    He implementado la forma que decia de movimiento y el salto del personaje de dos diferentes formas tambien.

--- projet-x/Assets/Scripts/Personaje/PJ1.cs
+++ projet-x/Assets/Scripts/Personaje/PJ1.cs
@@ -6,33 +6,42 @@ using UnityEngine;
 
 public class PJ1 : MonoBehaviour
 {
-    //lo eh quitado las fuerzas porque se lo damos fuera 
-    public float fuerza;
-    public float salto ;
+    //Le he quitado las fuerzas porque se lo damos fuera 
+    public float velocidad;
+    public float fuerzaSalto;
+    private bool salto = false;
+
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-       
+        rb = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
-    void FixedUpdate()
-    {
-
-        mover(fuerza);
+    void Update() {
         
     }
-
-    public void mover(float fuerza)
+    void FixedUpdate()
+    {
+        mover(velocidad * Time.deltaTime, fuerzaSalto);
+    }
+    public void mover(float vel, float salt)
     {
         if (Input.GetKey("a"))//Izquierda
         {
-            gameObject.transform.Translate(-fuerza * Time.deltaTime, 0, 0);
+            gameObject.transform.Translate(-vel, 0f, 0f);
+            //rb.velocity = new Vector2(-vel, rb.velocity.y);
         }
         else if (Input.GetKey("d"))//Derecha
         {
-            gameObject.transform.Translate(fuerza * Time.deltaTime, 0, 0);
+            gameObject.transform.Translate(vel, 0f, 0f);
+            //rb.velocity = new Vector2(vel, rb.velocity.y);
+        }
+        if (Input.GetKey("space"))//Salto
+        {
+            gameObject.transform.Translate(0f, salt *Time.deltaTime, 0f);
+            //rb.AddForce(new Vector2(0f, salt));
         }
     }
 
 }
-

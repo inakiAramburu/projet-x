c1e73ac0b5734168699674237625918e8e04cd5d
Iñaki Aramburu <inaki.aramburur@alumni.mondragon.edu>
2020-07-03 22:33:53 +0200

    disparo

--- projet-x/Assets/Scripts/Personaje/PJ1.cs
+++ projet-x/Assets/Scripts/Personaje/PJ1.cs
@@ -7,6 +7,16 @@ public class PJ1 : MonoBehaviour
 {
 
     int salto = 1;
+
+    [Header("Disparo")]
+    public GameObject disparo;
+    public Transform spawn_disparo_R;
+    public Transform spawn_disparo_L;
+   
+    public int cantidad_disparo;
+
+    public float direccion;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +26,38 @@ public class PJ1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //dispara a la derecha
+
+        if (Input.GetKey("e")&& gameObject.GetComponent<SpriteRenderer>().flipX == false && cantidad_disparo >0 )//disparar
+        {
+
+            Instantiate(disparo, spawn_disparo_R.position, spawn_disparo_R.rotation);
+          
+            
+
+           
+            cantidad_disparo -= 1;
+
+            
+
+        }
+        //dispara a la izquierda
+        else if (Input.GetKey("e") && gameObject.GetComponent<SpriteRenderer>().flipX == true && cantidad_disparo > 0)//disparar
+        {
+
+            Instantiate(disparo, spawn_disparo_L.position, spawn_disparo_L.rotation);
+        
+          
+
+            cantidad_disparo -= 1;
+
+            
+        }
+
+
+
+        ///////////////////////////////////////////movimiento///////////////////////////////////////////
+
         if (Input.GetKey("a"))
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-8700f * Time.deltaTime, 0));
@@ -26,24 +68,29 @@ public class PJ1 : MonoBehaviour
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(8700f * Time.deltaTime, 0));
             gameObject.GetComponent<SpriteRenderer>().flipX = false;
         }
-        if (Input.GetKeyDown("space") && salto<=1)
+        if (Input.GetKeyDown("space") && salto <= 1)
         {
-           
+
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1800f));
             salto += 1;
         }
-        if (Input.GetKey("s") )
+        if (Input.GetKey("s"))
         {
 
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -20f));
         }
-        if (salto>0)
+        if (salto > 0)
         {
             Debug.Log(salto);
-           // gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -4f));
+            // gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -4f));
 
         }
     }
+
+
+
+   
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.tag == "plataforma")
---
+++ projet-x/Assets/Scripts/Personaje/disparo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disparo : MonoBehaviour
+{
+
+    
+    public float velocidad;
+
+
+
+    public int direccion=1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       
+
+
+        disparar(direccion);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+         //velocidad *= direccion;
+    }
+
+
+    void disparar(int direccion)
+    {
+        velocidad *= direccion;
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(velocidad, 0.0f);
+
+    }
+}
